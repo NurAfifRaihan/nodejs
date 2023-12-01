b7b085394febe2db9399980ff3f7d57f10d58844
NurAfifRaihan <114470342+NurAfifRaihan@users.noreply.github.com>
2023-12-01 21:40:51 +0700

    praktikum9

---
+++ task/data/fruits.js
@@ -0,0 +1,9 @@
+/**
+ * TODO 1:
+ * - Buat array yang berisi data buah.
+ * - Refactor variable ke ES6 Variable.
+ */
+const fruits = ["melon", "semangka", "anggur", "pepaya"];
+
+// TODO 2: export variable fruits
+module.exports = fruits;
